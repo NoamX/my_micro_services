b4b343c8ac00b0720635e12ed1134978864792ce
Noam <dos-santos.noam@epitech.eu>
2020-11-24 23:17:02 +0100

    api_express

---
+++ api_express/controllers/DiscussionController.js
@@ -0,0 +1,19 @@
+const Discussion = require("../models/DiscussionSchema");
+const jwt = require("jsonwebtoken");
+
+const DiscussionController = {
+  create: (req, res) => {
+    //
+  },
+  read: (req, res) => {
+    //
+  },
+  update: (req, res) => {
+    //
+  },
+  remove: (req, res) => {
+    //
+  },
+};
+
+module.exports = DiscussionController;
---
+++ api_express/routes/DiscussionRoute.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const {
+  create,
+  read,
+  update,
+  remove,
+} = require("../controllers/DiscussionController");
+
+module.exports = router;
